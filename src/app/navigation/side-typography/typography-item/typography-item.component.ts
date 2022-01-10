606cd98c51a56239eafee9268901da7be8aad104
KayseMca <madadmca04@gmail.com>
2022-01-10 07:32:34 -0800

    typography item dynamically work

--- src/app/navigation/side-typography/typography-item/typography-item.component.ts
+++ src/app/navigation/side-typography/typography-item/typography-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-typography-item',
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypographyItemComponent implements OnInit {
 
+  @Input() index!:number
   constructor() { }
 
+  
   ngOnInit(): void {
   }
 
--- src/app/navigation/side-typography/typography-list/typography-list.component.ts
+++ src/app/navigation/side-typography/typography-list/typography-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-typography-list',
@@ -7,12 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypographyListComponent implements OnInit {
 
+  @Input() index!:number
+  show:Boolean = false
+  clicked = null
+  @HostListener('window:mousedown', ['$event'])
+  onMouseUp(event: any) {
+
+    let target = ['fa']
+    // let list = event.target.classList
+    // let value = (event.target as Element).className
+    // console.log(list)
+    // if(!target.includes(value)) this.openDropdown = false
+    let value = (event.target as Element).className
+    if (!target.includes(value)) {
+      // this.openTabs = false
+      this.show = false
+
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   openFont(){
+    this.show = false
     console.log("open the fonts")
+    this.show = !this.show
+    console.log(this.index)
   }
 }
