76f744df39778b8a0a34d79d8bc37bea286ef203
KayseMca <madadmca04@gmail.com>
2022-01-11 07:02:03 -0800

    toggled the typography list

--- src/app/navigation/side-typography/side-typography.component.ts
+++ src/app/navigation/side-typography/side-typography.component.ts
@@ -7,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideTypographyComponent implements OnInit {
 
+  toggleList:{[key:string]:boolean}  = {'typo':false, 'color':true}
+
   list = [1,3]
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.toggleList)
   }
 
+
+  toggleDesign(event:string){
+    if(event==='color'){
+      this.toggleList['typo'] = false
+      this.toggleList['color']=true
+    } else{
+      this.toggleList['color']=false
+      this.toggleList['typo'] = true
+    }
+  }
 }
