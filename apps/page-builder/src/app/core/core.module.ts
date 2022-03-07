3bfd41f5f23796152ae329ecb4890cfb601304e6
KayseMca <madadmca04@gmail.com>
2022-03-07 05:25:24 -0800

    added template loader service

--- apps/page-builder/src/app/core/core.module.ts
+++ apps/page-builder/src/app/core/core.module.ts
@@ -1,12 +1,28 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TemplateToEditService } from './service/template-to-edit.service';
 
+export function initTemplate(appInitService: TemplateToEditService) {
+  return () => { 
+    console.log("initliazing the template")
+    return appInitService.init();
+  }
+}
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers:[
+    TemplateToEditService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory:initTemplate,
+      deps:[TemplateToEditService],
+      multi: true
+     }
   ]
 })
 export class CoreModule { }
